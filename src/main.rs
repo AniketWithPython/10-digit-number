8b391b2d27522ffde55462034399ee79b54a95f2
AniketWithPython <98256334+AniketWithPython@users.noreply.github.com>
2024-07-12 16:18:20 +0530

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use rayon::prelude::*;
+
+fn countchar_iseq(a: char, s: &[char], i: usize) -> bool {
+    let num = s.iter().filter(|c| **c == a).count();
+
+    num == s[i].to_digit(10).unwrap().try_into().unwrap()
+}
+
+fn check(s: u128) {
+    let check: Vec<char> = s.to_string().chars().collect();
+
+    for i in 0..10 {
+        if !countchar_iseq(char::from_digit(i, 10).unwrap(), &check, i as usize) {
+            return;
+        }
+    }
+    println!("{}", s);
+}
+
+fn main() {
+    (1_000_000_000..10_000_000_000)
+        .into_par_iter()
+        .for_each(check);
+}
+
+// Answer is 6210001000
