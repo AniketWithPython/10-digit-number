8163543e9c50ca992a9a6081b73ce09ae7363ef8
AniketWithPython <98256334+AniketWithPython@users.noreply.github.com>
2024-07-12 18:56:25 +0530

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rayon::prelude::*;
+use std::time::Instant;
 
 fn countchar_iseq(a: char, s: &[char], i: usize) -> bool {
     let num = s.iter().filter(|c| **c == a).count();
@@ -9,7 +10,7 @@ fn countchar_iseq(a: char, s: &[char], i: usize) -> bool {
 fn check(s: u128) {
     let check: Vec<char> = s.to_string().chars().collect();
 
-    for i in 0..10 {
+    for i in 0..7 {
         if !countchar_iseq(char::from_digit(i, 10).unwrap(), &check, i as usize) {
             return;
         }
@@ -18,9 +19,11 @@ fn check(s: u128) {
 }
 
 fn main() {
-    (1_000_000_000..10_000_000_000)
-        .into_par_iter()
-        .for_each(check);
+    let now = Instant::now();
+
+    (1_000_000..10_000_000).into_par_iter().for_each(check);
+
+    println!("Time taken: {:?}", now.elapsed())
 }
 
 // Answer is 6210001000
